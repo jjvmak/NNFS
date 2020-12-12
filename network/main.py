d31b66c5dfc5c569e25445186e9fb22b147f1c35
Joonas Mäkinen <makinen.jns@gmail.com>
2020-12-12 09:49:00 +0200

    track inputs in relu and use backwards prob

--- network/main.py
+++ network/main.py
@@ -6,10 +6,11 @@ from network.layers import Layer_Dense
 from network.relu import Activation_ReLU
 from network.softmax import Activation_Softmax
 from network.loss_categorical_crossentropy import Loss_CategoricalCrossentropy
+from network.softmax_loss_categorical_crossentropy import Activation_Softmax_Loss_CategoricalCrossentropy
 
 nnfs.init()
 
-X, y = vertical_data(samples=100, classes=3)
+X, y = spiral_data(samples=100, classes=3)
 
 # 1 Layer and activation
 dense1 = Layer_Dense(2, 3)
@@ -17,51 +18,34 @@ activation1 = Activation_ReLU()
 
 # 2 Layer and activation
 dense2 = Layer_Dense(3, 3)
-activation2 = Activation_Softmax()
+loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
-loss_function = Loss_CategoricalCrossentropy()
+dense1.forward(X)
+activation1.forward(dense1.output)
 
-# Helper variables
-lowest_loss = 999999
-best_dense1_weights = dense1.weights.copy()
-best_dense1_biases = dense1.biases.copy()
-best_dense2_weights = dense2.weights.copy()
-best_dense2_biases = dense2.biases.copy()
+dense2.forward(activation1.output)
 
-# Very naive weight update routine
-for iteration in range(10000):
-    # Update network
-    dense1.weights += 0.05 * np.random.randn(2, 3)
-    dense1.biases += 0.05 * np.random.randn(1, 3)
-    dense2.weights += 0.05 * np.random.randn(3, 3)
-    dense2.biases += 0.05 * np.random.randn(1, 3)
+loss = loss_activation.forward(dense2.output, y)
 
-    dense1.forward(X)
-    activation1.forward(dense1.output)
-    dense2.forward(activation1.output)
-    activation2.forward(dense2.output)
+print(loss_activation.output[:5])
 
-    loss = loss_function.calculate(activation2.output, y)
+print('loss: ', loss)
 
-    # calculate accuracy
-    predictions = np.argmax(activation2.output, axis=1)
-    if len(y.shape) == 2:
-        y = np.argmax(y, axis=1)
-    # True evaluates to 1; False to 0
-    # correct predictions / n
-    accuracy = np.mean(predictions == y)
-
-    if loss < lowest_loss:
-        print('New set of weights found, iteration: ', iteration,
-              'loss: ', loss, 'acc: ', accuracy)
-        best_dense1_weights = dense1.weights.copy()
-        best_dense1_biases = dense1.biases.copy()
-        best_dense2_weights = dense2.weights.copy()
-        best_dense2_biases = dense2.biases.copy()
-        lowest_loss = loss
-    else:
-        # Revert back
-        dense1.weights = best_dense1_weights.copy()
-        dense1.biases = best_dense1_biases.copy()
-        dense2.weights = best_dense2_weights.copy()
-        dense2.biases = best_dense2_biases.copy()
+# Calculate accuracy from output of activation2 and targets
+# calculate values along first axis
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions == y)
+# Print accuracy
+print('acc:', accuracy)
+# Backward pass
+loss_activation.backward(loss_activation.output, y)
+dense2.backward(loss_activation.dinputs)
+activation1.backward(dense2.dinputs)
+dense1.backward(activation1.dinputs)
+# Print gradients
+print(dense1.dweights)
+print(dense1.dbiases)
+print(dense2.dweights)
+print(dense2.dbiases)
--- network/relu.py
+++ network/relu.py
@@ -3,6 +3,7 @@ import numpy as np
 
 class Activation_ReLU:
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
 
     def backward(self, dvalues):
