e3ba6a7263d39d96d0f22fbc8bb70b908776c3b9
Joonas Mäkinen <makinen.jns@gmail.com>
2020-12-12 09:14:41 +0200

    combined softmax and loss

---
+++ network/softmax_loss_categorical_crossentropy.py
@@ -0,0 +1,31 @@
+from network.softmax import Activation_Softmax
+from network.loss_categorical_crossentropy import Loss_CategoricalCrossentropy
+
+# Softmax classifier - combined Softmax activation
+# and cross-entropy loss for faster backward step
+class Activation_Softmax_Loss_CategoricalCrossentropy():
+
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossentropy()
+
+    def forward(self, inputs, y_true):
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
+
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+
+        # If labels are one-hot encoded,
+        # turn them into discrete values
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        self.dinputs = dvalues.copy()
+        # Calculate gradient
+        self.dinputs[range(samples), y_true] -= 1
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+
