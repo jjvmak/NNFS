a6d911c147ffd66f4c932fd9553a2ed728e3ab83
Joonas Mäkinen <makinen.jns@gmail.com>
2020-12-12 10:07:45 +0200

    training loop with optimizer

--- network/main.py
+++ network/main.py
@@ -7,45 +7,51 @@ from network.relu import Activation_ReLU
 from network.softmax import Activation_Softmax
 from network.loss_categorical_crossentropy import Loss_CategoricalCrossentropy
 from network.softmax_loss_categorical_crossentropy import Activation_Softmax_Loss_CategoricalCrossentropy
+from network.optimizer_sgd import Optimizer_SGD
 
 nnfs.init()
 
 X, y = spiral_data(samples=100, classes=3)
 
 # 1 Layer and activation
-dense1 = Layer_Dense(2, 3)
+dense1 = Layer_Dense(2, 64)
 activation1 = Activation_ReLU()
 
 # 2 Layer and activation
-dense2 = Layer_Dense(3, 3)
+dense2 = Layer_Dense(64, 3)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
-dense1.forward(X)
-activation1.forward(dense1.output)
+optimizer = Optimizer_SGD()
 
-dense2.forward(activation1.output)
+# Train in loop
+for epoch in range(10001):
+    # Perform a forward pass of our training data through this layer
+    dense1.forward(X)
+    # Perform a forward pass through activation function
+    # takes the output of first dense layer here
+    activation1.forward(dense1.output)
+    # Perform a forward pass through second Dense layer
+    # takes outputs of activation function of first layer as inputs
+    dense2.forward(activation1.output)
+    # Perform a forward pass through the activation/loss function
+    # takes the output of second dense layer here and returns loss
+    loss = loss_activation.forward(dense2.output, y)
 
-loss = loss_activation.forward(dense2.output, y)
-
-print(loss_activation.output[:5])
-
-print('loss: ', loss)
-
-# Calculate accuracy from output of activation2 and targets
-# calculate values along first axis
-predictions = np.argmax(loss_activation.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions == y)
-# Print accuracy
-print('acc:', accuracy)
-# Backward pass
-loss_activation.backward(loss_activation.output, y)
-dense2.backward(loss_activation.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward(activation1.dinputs)
-# Print gradients
-print(dense1.dweights)
-print(dense1.dbiases)
-print(dense2.dweights)
-print(dense2.dbiases)
+    # Calculate accuracy from output of activation2 and targets
+    # calculate values along first axis
+    predictions = np.argmax(loss_activation.output, axis=1)
+    if len(y.shape) == 2:
+        y = np.argmax(y, axis=1)
+    accuracy = np.mean(predictions==y)
+    if not epoch % 100:
+        print(f'epoch: {epoch}, ' +
+        f'acc: {accuracy:.3f}, ' +
+        f'loss: {loss:.3f}')
+    # Backward pass
+    loss_activation.backward(loss_activation.output, y)
+    dense2.backward(loss_activation.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
+    # Update weights and biases
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
